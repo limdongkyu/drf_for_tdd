9b8bce84cfd5d97777b5e7fd108f7691635873ac
limdongkyu <hacker004@nate.com>
2020-02-29 03:06:01 +0900

    url update

--- todos/urls.py
+++ todos/urls.py
@@ -5,5 +5,5 @@ app_name = 'todos'
 
 urlpatterns = [
     path('', TodoListCreateAPIView.as_view(), name="list"),
-    path('<int:pk>/', TodoDetailAPIView.as_view(), name="detail"),
+    path('<int:pk>', TodoDetailAPIView.as_view(), name="detail"),
 ]
--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from .views import UserRegistrationAPIView, UserLoginAPIView, UserLogoutView
 app_name = 'users'
 
 urlpatterns = [
-    path('users/', UserRegistrationAPIView.as_view(), name="list"),
-    path('users/login/', UserLoginAPIView.as_view(), name="login"),
-    path('users/logout/', UserLogoutView.as_view(), name="logout"),
+    path('users', UserRegistrationAPIView.as_view(), name="list"),
+    path('users/login', UserLoginAPIView.as_view(), name="login"),
+    path('users/logout', UserLogoutView.as_view(), name="logout"),
 ]
