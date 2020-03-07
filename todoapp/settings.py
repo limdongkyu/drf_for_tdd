9b0df0617ee7fbdf337565c83b9a2feac0606e2a
limdongkyu <hacker004@nate.com>
2020-03-08 04:13:35 +0900

    permisstion remove

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -72,9 +72,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
